262d342411807a134777cf9d2e4b1685a1e47391
wisdom5565 <104509621+wisdom5565@users.noreply.github.com>
2022-11-01 11:28:48 +0900

    Update DBConnection.java

--- src/dao/DBConnection.java
+++ src/dao/DBConnection.java
@@ -8,9 +8,7 @@ public class DBConnection {
 	private static Connection conn;
 	public static Connection getConnection() {
 		if(conn == null) {
-			String url = "jdbc:oracle:thin:@localhost:1521:XE";
-			String user = "web";
-			String password = "web";
+
 			try {
 				Class.forName("oracle.jdbc.driver.OracleDriver");
 				conn = DriverManager.getConnection(url,user,password);
